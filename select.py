f1304790fe9d2dd0e7877df0302e4692a34fa0a9
davidgamaserrate1 <86624625+davidgamaserrate1@users.noreply.github.com>
2021-10-07 20:52:14 -0300

    select.py

---
+++ select.py
@@ -0,0 +1,30 @@
+def parttition(A,p,r):
+  vetoresComMediana = []
+  tetoTamanhoVetor = len(A) / 5
+  mergeSort(A)
+  #atribuindo o teto do tamanho do array dividido por 5, a uma variavel
+  if tetoTamanhoVetor > 5:      
+    if tetoTamanhoVetor - round(tetoTamanhoVetor)  > 0 :
+        tetoTamanhoVetor = round(tetoTamanhoVetor) + 1
+    else:
+      tetoTamanhoVetor = (round(tetoTamanhoVetor))
+  j = 2
+  for j in range (tetoTamanhoVetor):
+    vetoresComMediana = A[j]
+    mergeSort(vetoresComMediana)
+
+  parttition(vetoresComMediana,p,r)
+
+def select (A, p, r ,i) :
+    if p == r:
+	    return A[p]
+    q = parttition(A, p,r)
+    k = q - p + 1
+
+    if k == i:
+        return A[q]
+    
+    if k > i  :  
+        select(A, p, q -1, i)
+    else:
+        select(A, q + 1, r, i-k)
